f407ca27970f69e6aaf9180930de2ced59dcae3b
rahul <rr200636@gmail.com>
2022-04-10 16:27:16 +0530

    add checklogin

--- gamma_tech/models.py
+++ gamma_tech/models.py
@@ -1,13 +1,5 @@
 from django.db import models
 
-# Create your models here.
-# class Candidate(models.Model):
-#     fname = models.CharField(max_length=100)
-#     lname = models.CharField(max_length=100)
-#     email = models.CharField(max_length=50)
-#     password = models.CharField(max_length=15)
-#     phone = models.IntegerField(max_length=10)
-#     faculty = models.BooleanField(default=True)
 
 class Candidate(models.Model):
     fname = models.CharField(max_length=100)
